1aa9c810448acd37124f94b207c82032800a2aee
God Johary <godjohary@gmail.com>
2023-01-25 22:19:31 +0300

    Disable JAR scanning

--- src/main/java/app/BackofficeVenteEnchereApplication.java
+++ src/main/java/app/BackofficeVenteEnchereApplication.java
@@ -1,8 +1,11 @@
 package app;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -25,6 +28,17 @@ public class BackofficeVenteEnchereApplication extends SpringBootServletInitiali
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatFactory() {
+        // disable JAR scanning
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                ((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
             }
         };
     }
